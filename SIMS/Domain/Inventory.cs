78d58b77e8d999b51f510ec3fc66d90f76535eee
Ibraheem Qadi <ibra444hem@gmail.com>
2025-04-07 09:55:32 +0300

    Add Inventory class to SIMS.Domain namespace

---
+++ SIMS/Domain/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS.Domain
+{
+    class Inventory
+    {
+        private List<Product> products = new List<Product>();
+    }
+}
